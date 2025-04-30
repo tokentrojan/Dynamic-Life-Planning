274a7ab6c2d2711c77e8fb4dac6c69e8d95b6348
JackM45 <jacksonmerrall@gmail.com>
2025-05-01 11:29:36 +1200

    Testing login functionality. Not working

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { auth } from '../firebase';
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  // const [navigate, setNavigate] = useNavigate();
+  const nav = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError('');
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      await signInWithEmailAndPassword(auth, email, password).then(() => nav(-1));
       // Redirect or perform actions after successful login
     } catch (err) {
         if (err instanceof Error) {
