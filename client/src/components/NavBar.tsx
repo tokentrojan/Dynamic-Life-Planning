680f527073c73b9264c802917bce9c2aa87a54fe
JackM45 <jacksonmerrall@gmail.com>
2025-05-01 19:24:23 +1200

    Added the logout button to the nav bar. Works as it should now
    There was no function to log the user out. I have added it in the navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,7 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase'; // adjust path if needed
+import { useNavigate } from 'react-router-dom';
 
 function AppNavbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate('/login');
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -10,6 +23,7 @@ function AppNavbar() {
           <NavLink className="nav-link" to="/planner">Planner</NavLink>
           <NavLink className="nav-link" to="/sorted">Sorted Tasks</NavLink>
           <NavLink className="nav-link" to="/unsorted">Unsorted Tasks</NavLink>
+          <button onClick={handleLogout} className="btn btn-link nav-link" style={{ padding: 0 }}>Logout </button>
         </Nav>
       </Container>
     </Navbar>
