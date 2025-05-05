675d5af5443a72648bb9efd4bc0671bcbcfe65bd
JackM45 <jacksonmerrall@gmail.com>
2025-05-05 13:34:07 +1200

    Adding db communication and tidied up the logic for current user

---
+++ client/src/AuthProvider.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./firebase"; // Adjust the path to your firebase.ts file
+import { AuthContext } from "./AuthContext"; // Import the context
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []); // Removed 'auth' from the dependency array
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- client/src/types/Task.ts
+++ client/src/types/Task.ts
@@ -6,4 +6,5 @@ export interface Task {
   dueDate: string; // ISO string format
   duration: number; // in minutes or hours
   priority: Priority;
+  recurring: boolean; //Set as either true or false for a recurring task
 }
