ca1b38e5ec09ec40ebf186ec428020b3bc481136
Zeyadbatis <zeyadbatis@gmail.com>
2025-05-17 15:36:49 +1200

    fixed an error in app
    completed was imported as completed and no Completed with capital c

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+
+    
+    react(),
+    VitePWA({
+      manifest: {
+
+        icons:[
+          {
+            src:"/dlp.png",
+            sizes:"512x512",
+            type:"image/png",
+            purpose:"any maskable"
+          }
+        ]
+
+      }
+
+    })
+
+  ],
 })
