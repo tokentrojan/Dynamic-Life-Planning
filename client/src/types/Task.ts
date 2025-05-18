18076843c15124c548ecc4ffeeb7b57597e89c7f
JackM45 <jacksonmerrall@gmail.com>
2025-05-19 11:23:01 +1200

    Added the do date to the task.
    Not yet showing on planner

--- client/src/types/Task.ts
+++ client/src/types/Task.ts
@@ -6,6 +6,7 @@ export interface Task {
   taskName: string;          // Required
   taskDescription: string;   // Required
   dueDate: string;           // ISO string (e.g., '2025-05-10T14:00')
+  doDate?: string;            // Same as dueDate, but used for calendat view. Optional (take due date if blank)
   completed: boolean;        // Required, default to false when adding tasks
   priority?: Priority;       // Optional
   duration?: number;         // Optional (minutes)
